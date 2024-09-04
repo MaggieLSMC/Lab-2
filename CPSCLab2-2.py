3748355b6e1eb75c63aaf9ce13b93362df034ce5
MaggieLSMC <164068965+MaggieLSMC@users.noreply.github.com>
2024-09-04 17:00:12 -0400

    Add files via upload

---
+++ CPSCLab2-2.py
@@ -0,0 +1,38 @@
+#the authors' names are: Maggie & Mac
+
+x = 3
+print(x)
+y = x*5 + x
+print("The calculated amount is", y)
+print(int(False))
+print(float(True))
+z = 1.2
+print(int(z))
+
+import math
+x = math.pi
+y = math.sin(x)
+z = x/4
+answer = 2 * y + 3**2 * math.cos(z)
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+print(answer)
+
+import math
+a = math.tan(math.pi / 4)
+numerator = math.sqrt(a + 1)
+b = math.tan(math.pi / 3)
+c = math.sin(math.pi / 3)
+denominator = math.sqrt(b + c)
+answer = numerator / denominator
+print(answer)
+
+import math
+r = 4
+a = 8 / math.sqrt(2)
+circle = math.pi * (r**2)
+square = a * a
+answer = circle - square
+print("The shadow area is", answer)
+
